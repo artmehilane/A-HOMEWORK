f512d0dfdab01e967f75572e33e33266f2917a66
artmehilane <124361380+artmehilane@users.noreply.github.com>
2023-02-28 16:46:25 +0200

    Art - Homework 1
    20. Valid Parentheses
    
    Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
    
    An input string is valid if:
    
    Open brackets must be closed by the same type of brackets.
    Open brackets must be closed in the correct order.
    Every close bracket has a corresponding open bracket of the same type.

---
+++ 1.1_homework_art.py
@@ -0,0 +1,55 @@
+
+
+"""""
+Valid Parentheses
+
+Given a string containing just the characters '(', ')', '{', '}', '[' and ']',
+determine if the input string is valid.
+
+Used language:
+Python 3.9
+
+Testing method:
+Running a line of code to test different input values with the newly created function
+
+Coding question from LeetCode:
+https://leetcode.com/problems/valid-parentheses/description/
+"""""
+
+
+
+
+def check_validity(input_string):
+
+    while input_string:
+
+        if "()" in input_string:
+            input_string = input_string.replace("()", "")
+
+        elif "[]" in input_string:
+            input_string = input_string.replace("[]", "")
+
+        elif "{}" in input_string:
+            input_string = input_string.replace("{}", "")
+
+        else:
+            return False
+    return True
+
+
+#Test
+
+tests = [
+    "[()[]{}]",
+    "{]",
+    "()",
+    "()[]{}",
+    "([{}])",
+    "[(])",
+    "{[()]}"
+]
+
+for test in tests:
+
+    print(check_validity(test))
+
