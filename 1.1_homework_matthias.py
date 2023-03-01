6baa223282d7097744420527fae902e934f6c52f
jacobspider <124361453+jacobspider@users.noreply.github.com>
2023-03-01 12:35:47 +0200

    Delete 1.1_homework_matthias.py

--- 1.1_homework_matthias.py
+++
@@ -1,5 +0,0 @@
-def countDigits(, num: int) -> int:
-        
-        numbrid = [int(d) for d in str(num) if d != '0']
-        lugeja = sum(1 for d in numbrid if num % d == 0)
-        return lugeja
